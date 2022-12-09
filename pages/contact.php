94089c6b0414368a307f340d0831d1233bb2fd0a
rodijanssen <74348802+rodijanssen@users.noreply.github.com>
2022-12-09 12:40:24 +0100

    contact.php

---
+++ pages/contact.php
@@ -0,0 +1,18 @@
+<?php
+include_once("../Assets/header.php");
+?>
+<div class="container">
+    <div class="card-header">
+        <div class="row align-items-center">
+            <div class="col-auto me-auto">
+                <h1>contact pagina</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<br />
+<div class="container">
+    <p>E-mail: contactdeblauweloper@gmail.com</p>
+    <p>telefoon nummer: +31 612893478</p>
+    <p>adress: straatlaan 35, cuijk 5431 AA</p>
+</div>
